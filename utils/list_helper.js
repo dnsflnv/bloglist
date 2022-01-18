b609f8eb01539ecd351a78a59d9e17a6dd635afe
Denis Filonov <denis@filonov.pro>
2022-01-18 20:05:49 +0100

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.reduce((acc, item) => {
+    return acc + item.likes
+  }, 0)
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
